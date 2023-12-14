4d953dc203fa3087fa4e77a306f6be365c0df6b4
진현규 <108057548+jinhyeonkyu@users.noreply.github.com>
2023-12-15 00:29:58 +0900

    #1 TODO "#3 람다식"

---
+++ src/자료구조/UnmodifiableEx.java
@@ -0,0 +1,19 @@
+package 자료구조;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+public class UnmodifiableEx {
+	public static void main(String[] args) {
+		System.out.println();
+
+	List<E> immutableList = List.copyOf()of(E...elements);
+	Set<E> immutableSet = Set.Copyof(E...elements);
+	Map<K,V> immutableMap = Map.Copyof(k k1, v v1, ... elements);
+
+
+
+
+	}
+}
