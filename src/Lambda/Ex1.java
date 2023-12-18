2fbc33efc1e9d706b3efb18302ff1955516e5370
진현규 <108057548+jinhyeonkyu@users.noreply.github.com>
2023-12-18 17:41:56 +0900

    TODO "Stream 메소드 알아보기"

---
+++ src/Lambda/Ex1.java
@@ -0,0 +1,9 @@
+package Lambda;
+
+
+public class Ex1 {
+	public static void main(String[] args) {
+
+	}
+
+}
--- src/자료구조/UnmodifiableEx.java
+++ src/자료구조/UnmodifiableEx.java
@@ -8,12 +8,5 @@ public class UnmodifiableEx {
 	public static void main(String[] args) {
 		System.out.println();
 
-	List<E> immutableList = List.copyOf()of(E...elements);
-	Set<E> immutableSet = Set.Copyof(E...elements);
-	Map<K,V> immutableMap = Map.Copyof(k k1, v v1, ... elements);
-
-
-
-
 	}
 }
