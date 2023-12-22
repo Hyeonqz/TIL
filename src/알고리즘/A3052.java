b0a8ac8927f420ddd101547a7c92e727954561a4
진현규 <108057548+jinhyeonkyu@users.noreply.github.com>
2023-12-22 21:11:47 +0900

    TODO : JVM, Backjoon

---
+++ src/알고리즘/A3052.java
@@ -0,0 +1,30 @@
+package 알고리즘;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class A3052 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int[] arr = new int[10];   //42로 나눌 숫자
+		int[] arr2 = new int[10];   //42로 나눈 나머지
+
+		for(int i=0;i<arr.length;i++) {
+			arr[i] = sc.nextInt();
+			arr2[i] = arr[i]%42;
+		}
+
+		int[] distinctNum = Arrays.stream(arr2)
+			.distinct()
+			.toArray();
+
+		System.out.println(distinctNum.length);
+
+
+
+	}
+}
