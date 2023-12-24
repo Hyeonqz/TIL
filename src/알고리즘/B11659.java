0fc2a518474a6dfcff3f7d2935ee5f034de4147f
진현규 <108057548+jinhyeonkyu@users.noreply.github.com>
2023-12-24 23:57:28 +0900

    TODO : "자료구조(배열,리스트)"

---
+++ src/알고리즘/B11659.java
@@ -0,0 +1,37 @@
+package 알고리즘;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B11659 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+		//데이터를 한줄로 쭉 받아올 때 사용
+		StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+		//int 값이므로 stringTokenizer를 파싱에서 받아와야함
+		int suNo = Integer.parseInt(stringTokenizer.nextToken());
+		int quizNo = Integer.parseInt(stringTokenizer.nextToken());
+
+		//배열 0번째 인덱스 무시하고 1부터 시작하게 하려고 +1을 함
+		long[] S = new long[suNo+1];
+
+		stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+		//합배열 구하기
+		for (int i = 1; i <=suNo; i++) {
+			S[i] = S[i-1] + Integer.parseInt(stringTokenizer.nextToken());
+		}
+
+		//구간합 구하기
+		for (int q = 0; q <quizNo ; q++) {
+			stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+			int i = Integer.parseInt(stringTokenizer.nextToken());
+			int j = Integer.parseInt(stringTokenizer.nextToken());
+			System.out.println(S[j] - S[i-1]);
+		}
+
+	}
+}
---
+++ src/알고리즘/B2018.java
@@ -0,0 +1,7 @@
+package 알고리즘;
+
+public class B2018 {
+	public static void main(String[] args) {
+
+	}
+}
