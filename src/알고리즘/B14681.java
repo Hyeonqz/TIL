4ab4d2c2cb98e4fa0e3c334e333f43b872b3af38
진현규 <108057548+jinhyeonkyu@users.noreply.github.com>
2023-12-27 16:24:29 +0900

    TODO : "조건문:알고리즘"

---
+++ src/알고리즘/B14681.java
@@ -0,0 +1,26 @@
+package 알고리즘;
+
+import java.util.Scanner;
+
+//사분면 고르기
+public class B14681 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+
+		if(x>0 && y>0) {
+			System.out.println("1");
+		}
+		if(x<0 && y>0) {
+			System.out.println("2");
+		}
+		if(x<0 && y<0) {
+			System.out.println("3");
+		}
+		if(x>0 && y<0) {
+			System.out.println("4");
+		}
+
+	}
+}
