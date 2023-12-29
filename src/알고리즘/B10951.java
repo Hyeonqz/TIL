a33dde2505f3daedd1fd9192bd8415f5c7d22ce9
진현규 <108057548+jinhyeonkyu@users.noreply.github.com>
2023-12-29 14:24:53 +0900

    TODO : "백준 : 반복문 끝"

---
+++ src/알고리즘/B10951.java
@@ -0,0 +1,7 @@
+package 알고리즘;
+
+public class B10951 {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/알고리즘/B15552.java
@@ -0,0 +1,25 @@
+package 알고리즘;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class B15552 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+		int T = Integer.parseInt(stringTokenizer.nextToken());
+
+		for (int i = 0; i <T ; i++) {
+			stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+			bufferedWriter.write((Integer.parseInt(stringTokenizer.nextToken()) + Integer.parseInt(stringTokenizer.nextToken()))+"\n");
+		}
+		bufferedWriter.close();
+	}
+}
