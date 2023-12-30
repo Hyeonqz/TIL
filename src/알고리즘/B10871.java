9aadc85ac033d32620cc6f14b7ba6d8b5c8aad1a
진현규 <108057548+jinhyeonkyu@users.noreply.github.com>
2023-12-30 16:02:53 +0900

    TODO : "백준 : 배열"

---
+++ src/알고리즘/B10871.java
@@ -0,0 +1,29 @@
+package 알고리즘;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B10871 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// 첫 번째 행을 읽어와서 N과 X를 얻음
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+
+		int[] arr = new int[N];
+
+		// 두 번째 행을 한 번에 읽어와서 배열을 초기화
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+
+			if (X > arr[i]) {
+				System.out.print(arr[i] + " ");
+			}
+		}
+	}
+}
