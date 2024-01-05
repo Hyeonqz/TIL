887f66d7081fba53c19d267de688b6f7d96601fc
진현규 <108057548+jinhyeonkyu@users.noreply.github.com>
2024-01-05 15:44:31 +0900

    TODO : "1차원 배열 끝"

---
+++ src/알고리즘/B26711.java
@@ -0,0 +1,16 @@
+package 알고리즘;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B26711 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(bf.readLine());
+		int M = Integer.parseInt(bf.readLine());
+
+		System.out.println(N + M);
+	}
+}
