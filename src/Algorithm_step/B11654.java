5c863b4e40b6f08afdfde05e7b1b0f65da4758cf
진현규 <108057548+jinhyeonkyu@users.noreply.github.com>
2024-01-06 16:50:23 +0900

    TODO : "Step 5"

---
+++ src/Algorithm_step/B11654.java
@@ -0,0 +1,15 @@
+package Algorithm_step;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B11654 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		String input = bf.readLine();
+		int ascii = (int)input.charAt(0);
+		System.out.println(ascii);
+	}
+}
