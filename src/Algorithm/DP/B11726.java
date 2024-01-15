6cb1e52842575390b242293139711950a5cec0db
진현규 <108057548+jinhyeonkyu@users.noreply.github.com>
2024-01-15 17:38:16 +0900

    TODO : "DP 알고리즘2"

---
+++ src/Algorithm/DP/B11726.java
@@ -0,0 +1,12 @@
+package Algorithm.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B11726 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+	}
+}
