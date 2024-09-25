9842580335391402b8cb4a672d6afea91675ddf2
hkjin <wlsgusrb78@naver.com>
2024-09-25 15:31:43 +0900

    파일정리

--- Java & OOP/Java/src/After_Java8/Lambda/ExInter.java
+++
@@ -1,7 +0,0 @@
-package Java.Lambda;
-
-@FunctionalInterface
-public interface ExInter {
-	public void calculate(int x, int y);
-
-}
--- Java & OOP/Java/src/DataStructure/자료구조/UnmodifiableEx.java
+++
@@ -1,8 +0,0 @@
-package DataStructure.자료구조;
-
-public class UnmodifiableEx {
-	public static void main(String[] args) {
-		System.out.println();
-
-	}
-}
